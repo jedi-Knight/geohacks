119794b68691c45becee4c868249e9e15efc3c5e
jediKnight <gautamxpratik@gmail.com>
2014-09-16 16:48:15 +0545

    pointClusters;

--- OSMEditsQuickOverlay/js/actions.js
+++ OSMEditsQuickOverlay/js/actions.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         height: $(document).innerHeight() - 20
     });
     var map = L.map('map', {
-        center: [27.71536, 85.34891],
+        center: [27.71536, 85.34841],
         zoom: 15,
         doubleClickZoom: true
     });
